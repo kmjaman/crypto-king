7789e79c367d4903d766d0d42e32afdb175fa49c
Jaman <jaman.edux@gmail.com>
2021-08-15 22:39:27 +0200

    added firebase

---
+++ wallet/lib/ui/authentication.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({ Key? key }) : super(key: key);
+
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+        ),
+        child: Text('Hello Crypto'),
+      ),
+    );
+  }
+}
