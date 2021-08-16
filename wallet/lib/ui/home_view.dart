61be505c32aaa1aff9643f936c4b46e9c4223ee8
Jaman <jaman.edux@gmail.com>
2021-08-16 03:59:06 +0200

    added listview

--- wallet/lib/ui/home_view.dart
+++ wallet/lib/ui/home_view.dart
@@ -1,5 +1,6 @@
 
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -12,9 +13,43 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home View'),
-      
-    );
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+              .collection('Users')
+              .doc(FirebaseAuth.instance.currentUser!.uid)
+              .collection('Coins')
+              .snapshots(),
+            builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData){
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView(
+                  children: snapshot.data!.docs.map((document) {
+                    return Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Coin Name: ${document.id}'),
+                          Text('Amount owned: ${document.data()}'),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              }
+            ),
+          
+          ),
+        ),
+      );
+    
   }
 }
