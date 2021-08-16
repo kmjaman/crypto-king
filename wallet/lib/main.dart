9aae1f253ab40a4a17e01cb5677cd2f9c221a781
Jaman <jaman.edux@gmail.com>
2021-08-16 03:16:00 +0200

    added login-register

--- wallet/lib/main.dart
+++ wallet/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:wallet/ui/authentication.dart';
 
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Crypto King',
       home: Authentication(),
     );
   }
--- wallet/lib/ui/authentication.dart
+++ wallet/lib/ui/authentication.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wallet/net/flutterfire.dart';
+import 'package:wallet/ui/home_view.dart';
 
 class Authentication extends StatefulWidget {
   const Authentication({ Key? key }) : super(key: key);
@@ -8,6 +10,9 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +22,82 @@ class _AuthenticationState extends State<Authentication> {
         decoration: BoxDecoration(
           color: Colors.blueAccent,
         ),
-        child: Text('Hello Crypto'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextFormField(
+              controller: _emailField,
+              decoration: InputDecoration(
+                hintText: 'example@email.com',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                )
+              ),
+            ),
+            TextFormField(
+              controller: _passwordField,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'password',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                )
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.5,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: MaterialButton(onPressed: () async{
+                bool shouldNavigate =
+                    await register(_emailField.text, _passwordField.text);
+                if (shouldNavigate) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeView(),
+                    ),
+                  );
+                }
+              },
+              child: Text('Register'),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.5,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: MaterialButton(onPressed: () async{
+                bool shouldNavigate =
+                    await signIn(_emailField.text, _passwordField.text);
+                if (shouldNavigate) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeView(),
+                    ),
+                  );
+                }
+              },
+              child: Text('Login'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
